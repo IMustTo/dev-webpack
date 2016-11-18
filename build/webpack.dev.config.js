cecc40954a454187ab9306910408b5d1a3ce7f9b
IMustTo <cxjbinfo@gmail.com>
2016-11-18 21:13:12 +0800

    todo css

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -11,6 +11,12 @@ module.exports = merge(baseConfig, {
     hot: true,
     inline: true,
     progress: true,
+    // proxy: {
+    //   '/core-web/*': {
+    //       target: 'http://manager.i3618.com.cn',
+    //       secure: false,
+    //   },
+    // },
   },
 
   devtool: 'eval-source-map',
